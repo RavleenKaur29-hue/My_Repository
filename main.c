540b54ca326439e9aaa44bb665908b375aa59cdd
ravleen kaur <ravleen0680.becse24@chitkara.edu.in>
2025-03-21 00:13:48 +0530

    Removed Addition Added Subtraction abd Multipication in main.c file

--- main.c
+++ main.c
@@ -3,7 +3,8 @@ int main()
 {
     int a,b;
     printf("enter two numbers:");
-    scanf("%d%d",&a,&b);
-    printf("sum:%d",a+b);
+    scanf("%d %d",&a,&b);
+    printf("Difference : %d\n",a-b);
+    printf("Muliplication : %d\n",a*b);
     return 0;
 }
